054d0feb5fe87178bf783e35c7b2abe22b7fec13
Musa Jabbaaru Ntege <cenajab@gmail.com>
2018-11-23 21:30:27 +0300

    update

--- iReporter/static/main.js
+++
@@ -1,9 +0,0 @@
-function functionAlert(msg, myYes) {
-    var confirmBox = $("#alert");
-    confirmBox.find(".message").text(msg);
-    confirmBox.find(".yes").unbind().click(function() {
-       confirmBox.hide();
-    });
-    confirmBox.find(".yes").click(myYes);
-    confirmBox.show();
- }
