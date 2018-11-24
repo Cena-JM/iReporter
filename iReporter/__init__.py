d999585e5eacc015589537ab99de3eb46da5fb9c
Musa Jabbaaru Ntege <cenajab@gmail.com>
2018-11-24 23:25:32 +0300

    master branch cleared

--- iReporter/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_login import LoginManager
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'a25a89a56f653a13deb01aa2368cab8c'
-
-
-from iReporter import routes
