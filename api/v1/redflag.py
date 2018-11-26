719909286c5e72446f0cbc1783333455cc7eba59
Musa Jabbaaru Ntege <cenajab@gmail.com>
2018-11-27 00:22:41 +0300

    Updated improved constraints for data integrity

--- api/v1/redflag.py
+++ api/v1/redflag.py
@@ -19,9 +19,17 @@ users = [
 ]
 redflag_records = [
     {
+        "rf_id": '1',
         "author": "JabMN",
         "title": "My first red flag title",
         "content": "This is first demo red flag content",
+        "date": "yy/mm/dd"
+    },
+    {
+        "rf_id": '2',
+        "author": "TestUser",
+        "title": "My second red flag title",
+        "content": "This is second demo red flag content",
         "date": "yy/mm/dd"
     }
 ]
@@ -29,26 +37,37 @@ redflag_records = [
 
 class Redflag(Resource):
     
-    def create_redflag_record(self, author):
+    def post(self, redflag):
         parser = reqparse.RequestParser()
+        parser.add_argument("author")
         parser.add_argument("title")
         parser.add_argument("content")
         args = parser.parse_args()
 
-        if author not in users:
-            return "User with name {} is not registered!".format(author), 400
+        try:
+            int(redflag)
+        except:
+            return "redflag argument should be an integer for an id", 406
+        else:
+            for redflag_record in redflag_records:
+                if(redflag == redflag_record["rf_id"]):
+                    return "redflag_record with id {} already exists".format(redflag), 400
 
-        redflag_record = {
-            "author": author,
-            "title": args["title"],
-            "content": args["content"],
-            "date": "yy/mm/dd"
-        }
+            redflag_record = {
+                "rf_id": redflag,
+                "author": args["author"],
+                "title": args["title"],
+                "content": args["content"],
+                "date": "yy/mm/dd"
+            }
 
-        redflag_records.append(redflag_record)
-        return redflag_record, 201
+            for username in users:
+                if redflag_record["author"] == username["username"]:
+                    redflag_records.append(redflag_record)
+                    return redflag_record, 201
+                return "User with name {} is not registered!".format(redflag_record["author"]), 400
 
     
-api.add_resource(Redflag, "/user/<string:name>")
+api.add_resource(Redflag, "/redflag/<string:redflag>")
 
 app.run(debug=True)
