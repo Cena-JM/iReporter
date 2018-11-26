95b0dce0fb0bc4136d18e64dc21d40478711ff94
Musa Jabbaaru Ntege <cenajab@gmail.com>
2018-11-26 22:23:47 +0300

    All get methods for both specific and all records

--- api/v1/redflag.py
+++ api/v1/redflag.py
@@ -19,9 +19,17 @@ users = [
 ]
 redflag_records = [
     {
+        "rf_id": '1',
         "author": "JabMN",
         "title": "My first red flag title",
         "content": "This is first demo red flag content",
+        "date": "yy/mm/dd"
+    },
+    {
+        "rf_id": '2',
+        "author": "TestUser",
+        "title": "My second red flag title",
+        "content": "This is second demo red flag content",
         "date": "yy/mm/dd"
     }
 ]
@@ -29,6 +37,16 @@ redflag_records = [
 
 class Redflag(Resource):
     
-    pass
+    def get(self, redflag = 'all'):
+        if redflag == 'all':
+            return redflag_records, 200
+        else:
+            for redflag_record in redflag_records:
+                if(redflag == redflag_record["rf_id"]):
+                    return redflag_record, 200
+            return "redflag_record is not found", 404
+
+    
+api.add_resource(Redflag, "/redflag/<string:redflag>")
 
 app.run(debug=True)
