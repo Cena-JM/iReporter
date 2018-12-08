b09731187dd2cec04bad557da5dcd22726767e10
Musa Jabbaaru Ntege <cenajab@gmail.com>
2018-12-09 00:20:49 +0300

    updated fuctionality

--- iReporter/ui/js/main.js
+++ iReporter/ui/js/main.js
@@ -7,6 +7,26 @@ function functionAlert(msg, myYes) {
     confirmBox.find(".yes").click(myYes);
     confirmBox.show();
  }
- function deleteAlert() {
+function deleteAlert() {
    alert("Are you sure you want this entry deleted?");
+}
+
+function validateForm() {
+   var usr_email = document.loginform.email.value;
+   var usr_password = document.loginform.password.value;
+   var email = "jabmn@ireporter.com";
+   var admin_email = "admin@ireporter.com";
+   var password = "password";
+   if ((usr_email == email) && (usr_password == password)) {
+      window.location.replace("account.html");
+      return false;
+   }
+   else if ((usr_email == admin_email) && (usr_password == password)) {
+      window.location.replace("admin.html");
+      return false;
+   }
+   else {
+      alert ("Login was unsuccessful, please check your username and password");
+      return false;
+   }
 }
